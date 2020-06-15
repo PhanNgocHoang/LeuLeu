0f2a3937f98ee79444e309e8da377dc2009d3b05
Trung updated <ngtrung4520@gmail.com>
2020-06-15 20:09:14 +0700

    Update compoment.index.js

--- compoments/compoment.index.js
+++ compoments/compoment.index.js
@@ -1,50 +1,64 @@
 const componentIndex = {
-    navIndex: `
-    <nav class="auth">
-    <button class="login selected" id="login1">Login</button>
-    <button class="signup" id="register1">Signup</button>
-    </nav>
-    `,
     login: `
-    <section>
-    <div id='login'>
-        <div>
-            <label for="">Username</label>
-            <input type="text"/>
-        </div>
-        <div>
-            <label for="">Password</label>
-            <input type="password" />
-        </div>
-        <div>
-            <div class="input-select">
-                <input id="remember-me" type="checkbox" />
-                <label for="remember-me">Remember me</label>
-            </div>
-        </div>
-        <div>
-            <button>Log in</button>
-        </div>
-    </section>
-    `,
+      <div class="container">
+          <nav class="auth">
+              <div class="signup">Login</div>
+          </nav>
+          <main>
+              <div id='login' class="show">
+                  <div>
+                      <div>
+                          <input type="email" name="email" placeholder="Email or Username">
+                          <div class="message-error" id="email-error"></div>
+                      </div>
+                      <div>
+                          <input type="password" name="password" placeholder="Password">
+                          <div class="message-error" id="password-error"></div>
+                      </div>
+                  </div>
+                  <div>
+                      <div class="input-select">
+                          <input id="remember-me" type="checkbox" />
+                          <label for="remember-me">Remember me</label>
+                      </div>
+                  </div>
+                  <div id="login-success" class="login-success"></div>
+                  <div id="login-error" class="login-error"></div>
+                  <div>
+                      <a href="#">Not yet have an account? Register</a>
+                      <button id="login-btn">Log in</button>
+                  </div>
+              </div>
+          </main>
+      </div>`,
     register: `
-    <section>
-<div id='signup'>
-    <div>
-        <label for="">Username</label>
-        <input type="text"/>
-    </div>
-    <div>
-        <label for="">Password</label>
-        <input type="password" /></div>
-    <div>
-    <div>
-        <label for="">Confirm Password</label>
-        <input type="password" /></div>
-    <div>
-        <button>Sign up</button>
-    </div>
-</div>
-</section>
-    `
-}
+      <div class="container">
+          <nav class="auth">
+              <div class="signup">Register</div>
+          </nav>
+          <main>
+              <div id='signup' class="show">
+                  <div>
+                      <input type="email" name="email" placeholder="Email or Username">
+                      <div class="message-error" id="email-error"></div>
+                  </div>
+                  <div>
+                      <input type="password" name="password" placeholder="Password">
+                      <div class="message-error" id="password-error"></div>
+                  </div>
+                  <div>
+                      <input type="password" name="confirm-password" placeholder="Confirm password">
+                      <div class="message-error" id="confirm-password-error"></div>
+                  </div>
+                  <div id="register-success" class="message-success"></div>
+                  <div id="register-error" class="message-error"></div>
+                  <div>
+                      <a href="#">Already have an account? Login</a>
+                      <button id="signup-btn">Sign up</button>
+                  </div>
+              </div>
+          </main>
+      </div>
+      `,
+  };
+  
